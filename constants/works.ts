b449ce2852bca07aaae772e474a2f0e42671d265
LeoDELPON <delponleo@gmail.com>
2022-02-23 22:48:55 +0100

    Content added

--- constants/works.ts
+++ constants/works.ts
@@ -3,15 +3,15 @@ export const works = [
     category: "Free-lance Project",
     title: "MyDemenageur",
     img: "/MyDemenageur.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
+    content: "A web app for service asking. First of all you have to login or signup to access to the different services provided by users. I developed during this mission a dynamic form generator that dislay a custom form for each service we have or want to create. I also did a lot of SEO using javascript's schemas.",
     tags:["Next.js", "Redux", "Typescript", ".NET Core", "WebSockets", "Redux-Saga"],
-    source: "/mydemenageur.com",
+    source:null,
   },
   {
     category: "Free-lance Project",
     title: "Trendex",
     img: "/Trendex.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
+    content: "During this mission, I mostly implemented frontend mockup using SCSS, Tailwind and React in typescript. I also created an SMTP microservice that would send an email for notifications. Thus, I did the in18 translation from french to english.",
     tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
     source:"https://app.trendex.vip/"
   },
@@ -19,48 +19,48 @@ export const works = [
     category: "Free-lance Project",
     title: "Axens",
     img: "/axens.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "A web app for visualizing data using graphs. I had to analyze recolted data using regressions methods (linear, poynomial, power, exponential, Theil-sen, and strong polynomial) and regression graph rendering. This web app was made to be a Grafana plugin.",
+    tags:["React.js", "Typescript", "ML.js", "Redux", "Visx Airbnb", "Material-UI"],
     source:"https://app.trendex.vip/"
   },
   {
     category: "Free-lance Project",
     title: "Accoustic Wells",
     img: "/accoustic-wells.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "A web app for visualizing data using graphs. I had to implement endpoints in backend repescting REST norms and using CRUD method. I also had to get the data from the endpoint with frontend axios process.",
+    tags:["React.js", ".NET Core", "Next.js", "C#"],
     source:"https://www.acoustic-wells.com/"
   },
   {
     category: "Free-lance Project",
     title: "SLP Foundation",
     img: "/SLP.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "I had to develop the all SLP Foundation website from a specific mockup. This site is static and fully responsive.",
+    tags:["React.js", "Next.js", "TailwindCSS", "Netlify"],
     source:"https://slp-new.netlify.app/"
   },
   {
     category: "Free-lance Project",
     title: "Blitz Network",
     img: "/blitz.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "This application is a landing page website with a simple form process. All data are stored and get with netlify scrapper. This site is static and fully responsive",
+    tags:["React.js", "Next.js", "TailwindCSS", "Netlify"],
     source:"https://blitznetwork.netlify.app/"
   },
   {
     category: "Free-lance Project",
     title: "Axa Banque",
     img: "/axa.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "Réussir is a group from Axa that manage all axa's brokers. I had to develop a dynamic organigram and a yearbook with all data got from the backend aplication. I also developed front and backend process for the application's dashboard that would manage the whole website. (CMS like)",
+    tags:["Next.js", "Typescript", "Blitz.js", "Prisma", "React-hook-form", "TailwindCSS"],
     source:null
   },
   {
     category: "Free-lance Project",
     title: "@Naute",
     img: "/naut.png",
-    content: "A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.",
-    tags:["React.js", "Typescript", "Node.js", "Redux", "Redux-Thunk"],
+    content: "A portfolio for a graphic designer. It contains an instagram's profile like with zoom in carousel system.",
+    tags:["React.js", "TailwindCSS", "SCSS"],
     source:null
   }
 ]

b5300df066b09f5d3507ddb25054868b317da1ab
LeoDELPON <delponleo@gmail.com>
2022-03-29 15:24:41 +0200

    Yo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import type { NextPage } from "next";
-import RootComponent from "../component/root";
+import type { NextPage } from 'next';
+import RootComponent from '../component/root';
 
-import Header from "../component/organism/header/Header";
-import About from "../component/organism/about";
-import Experience from "../component/organism/experience";
-import GetInTouch from "../component/organism/contact/getInTouch";
-import Work from "../component/organism/work";
+import Header from '../component/organism/header/Header';
+import About from '../component/organism/about';
+import AboutFriend from '../component/organism/about';
+import Experience from '../component/organism/experience';
+import GetInTouch from '../component/organism/contact/getInTouch';
+import Work from '../component/organism/work';
 
 const Home: NextPage = () => {
   return (
     <RootComponent>
       <Header />
       <About />
+      <AboutFriend />
       <Experience />
       <Work />
       <GetInTouch />
